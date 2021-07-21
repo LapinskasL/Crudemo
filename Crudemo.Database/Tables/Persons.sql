5260f52ffe5b68b8c1fb51ed73d147c96b3d2b2b
Lukas Lapinskas <lapinskasl3@gmail.com>
2021-07-20 21:01:20 -0500

    added Persons table

---
+++ Crudemo.Database/Tables/Persons.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Persons]
+(
+	[Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]   NVARCHAR (50) NOT NULL,
+    [LastName]    NVARCHAR (50) NOT NULL,
+    [PhoneNumber] NVARCHAR (20) NOT NULL,
+    [AddressId]   INT           NOT NULL,
+    [ConcurrencyToken] ROWVERSION NOT NULL,
+    [DateAdded] DATETIME2(7) DEFAULT(GETDATE()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+)
