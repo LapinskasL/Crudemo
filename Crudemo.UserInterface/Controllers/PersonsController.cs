39a63881ba9c49063f53ead6dab13e4fdecab0a2
Lukas Lapinskas <lapinskasl3@gmail.com>
2021-07-25 17:31:55 -0500

    added person model and GetPersons action with mock data

--- Crudemo.UserInterface/Controllers/PersonsController.cs
+++ Crudemo.UserInterface/Controllers/PersonsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Crudemo.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,25 @@ namespace Crudemo.WebUI.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetPersons()
+        {
+            List<PersonModel> persons = new List<PersonModel>();
+            for (int i = 0; i < 30; i++)
+            {
+                persons.Add(new PersonModel
+                {
+                    Id = i + 1,
+                    FirstName = "First" + i.ToString(),
+                    LastName = "Last" + i.ToString(),
+                    PhoneNumber = "123456789" + i.ToString(),
+                    ConcurrencyToken = "",
+                    DateAdded = DateTime.Now
+                });
+            }
+
+            return Json(new { data = persons });
         }
     }
 }
---
+++ Crudemo.UserInterface/Models/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crudemo.WebUI.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ConcurrencyToken { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
