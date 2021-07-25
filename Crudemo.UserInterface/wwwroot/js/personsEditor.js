35fd86e12d0884278f64aa4e187d3a98fbacd5bb
Lukas Lapinskas <lapinskasl3@gmail.com>
2021-07-25 18:48:43 -0500

    added bootbox modal popup for create button

--- Crudemo.UserInterface/wwwroot/js/personsEditor.js
+++ Crudemo.UserInterface/wwwroot/js/personsEditor.js
@@ -11,4 +11,23 @@
             { data: "dateAdded"}
         ]
     });
+    return table;
+}
+
+var tempHtmlForm = '<form> <div class="form-group"> <label for="formGroupExampleInput">Example label</label> <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input"> </div> <div class="form-group"> <label for="formGroupExampleInput2">Another label</label> <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input"> </div> </form>';
+function OpenCreateModal() {
+    bootbox.dialog({
+        title: 'Create User',
+        size: 'small',
+        message: tempHtmlForm,
+        buttons: {
+            submit: {
+                label: 'Submit',
+                className: 'btn-primary',
+                callback: function () {
+                    alert('submitted');
+                }
+            }
+        }
+    })
 }
