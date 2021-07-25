f1fd6aa025c348dc7171c39439563cb2276c578f
Lukas Lapinskas <lapinskasl3@gmail.com>
2021-07-25 18:04:29 -0500

    added datatable retrieving mock persons data

--- Crudemo.UserInterface/Models/PersonModel.cs
+++ Crudemo.UserInterface/Models/PersonModel.cs
@@ -8,8 +8,8 @@ namespace Crudemo.WebUI.Models
     public class PersonModel
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FirstName { get; set; }
         public string PhoneNumber { get; set; }
         public string ConcurrencyToken { get; set; }
         public DateTime DateAdded { get; set; }
---
+++ Crudemo.UserInterface/wwwroot/js/personsEditor.js
@@ -0,0 +1,14 @@
+﻿function InitializePersonsDataTable() {
+    let table = $('#personsTable').DataTable({
+        ajax: {
+            url: '/persons/getPersons',
+            type: 'GET'
+        },
+        columns: [
+            { data: "lastName" },
+            { data: "firstName" },
+            { data: "phoneNumber" },
+            { data: "dateAdded"}
+        ]
+    });
+}
