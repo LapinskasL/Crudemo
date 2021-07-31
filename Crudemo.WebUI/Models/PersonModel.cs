4d3a00d6970f12b05151601c96b3f71dd96d49b9
Lukas Lapinskas <lapinskasl3@gmail.com>
2021-07-31 11:26:43 -0500

    added validation to model

--- Crudemo.WebUI/Models/PersonModel.cs
+++ Crudemo.WebUI/Models/PersonModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,17 @@ namespace Crudemo.WebUI.Models
     public class PersonModel
     {
         public int Id { get; set; }
-        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public string ConcurrencyToken { get; set; }
         public DateTime DateAdded { get; set; }
